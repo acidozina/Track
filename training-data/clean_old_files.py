e957f86e519169ff6e63f03e597908bfcbd4a6b0
Vadim Krupkin <vadim.krupkin@streamlabs.ru>
2022-06-20 11:27:10 +0300

    training data

---
+++ training-data/clean_old_files.py
@@ -0,0 +1,17 @@
+﻿import os
+from datetime import datetime
+
+folder = "IMG"
+filelist = os.listdir(folder)
+
+start = datetime(2022,6,20,10,34,49);
+
+n_removed = 0
+for name in filelist:
+	cam, y,m,d,h,mm,ss,rest = name.split("_")
+	curr = datetime(int(y),int(m),int(d),int(h),int(mm),int(ss))
+	if curr < start :
+		os.remove("{}//{}".format(folder,name))
+		n_removed = n_removed + 1
+
+print("{} files deleted".format(n_removed))
